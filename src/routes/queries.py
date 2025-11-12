8bb91e05bc25cfcc0d2ad105ecceba6e6f4424c7
kmenshikoff <kmenshikoff@itba.edu.ar>
2025-11-12 19:09:47 -0300

    feat: Add additional Neo4j query endpoints (q3, q4, q5, q11, q12)
    Co-authored-by: Javier Peral <112484112+za0sec@users.noreply.github.com>
    Co-authored-by: Tomas Pinausig <112183921+pi-na@users.noreply.github.com>
    Co-authored-by: asquillari <136518507+asquillari@users.noreply.github.com>

--- src/routes/queries.py
+++ src/routes/queries.py
@@ -9,3 +9,19 @@ def q1(limit: int = Query(100, ge=1, le=1000)):  return mongo_q.q1(limit)
 @router.get("/q2")
 def q2(limit: int = Query(100, ge=1, le=1000)):  return mongo_q.q2(limit)
 
+#NEO4J QUERIES
+
+@router.get("/q3")
+def q3():  return neo_q.q3()
+
+@router.get("/q4")
+def q4():  return neo_q.q4()
+
+@router.get("/q5")
+def q5():  return neo_q.q5()
+
+@router.get("/q11")
+def q11(): return neo_q.q11()
+
+@router.get("/q12")
+def q12(): return neo_q.q12()
